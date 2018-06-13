a53168a277714fda4062d0e8802f2873badce86b
EdBrons <ianebrons@gmail.com>
2018-06-13 12:33:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ io.sockets.on("connection", function(socket){
 	factions[socket.id] = {};
 	factions[socket.id].color = socket.color;
 	factions[socket.id].points = 100;
-	factions[socket.id].pointIncome = .1;
+	factions[socket.id].pointIncome = 1;
 	
 	var position = getRandomPosition();
 	if (position != false){
