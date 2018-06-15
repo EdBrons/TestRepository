80f9f420c265354a54246a3378ef44a3aac50fbf
EdBrons <ianebrons@gmail.com>
2018-06-15 10:57:20 -0400

    Update map.js

--- server/map.js
+++ server/map.js
@@ -82,7 +82,7 @@ Map.prototype.update = function(){
 
 		if (unit.moving){
 			if (this.canMoveTo(unit, unit.destination)){
-				unit.progress += 30;
+				unit.progress += 10;
 				if (unit.progress > 100){
 
 					//tell ian for testing purposes
